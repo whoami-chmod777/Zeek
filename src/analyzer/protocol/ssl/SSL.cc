2c45055dee1403951cb38f9d21d53893d65db82d
Evan Typanski <evan.typanski@corelight.com>
2024-10-29 09:48:04 -0400

    Fix Clang 19 deprecation failure
    Fixes #3994
    
    Clang 19 with libc++ started failing to compile because the default
    implementation of `std::char_traits` was removed, making uses of
    `std::char_traits<unsigned int>` invalid. This was more of used for
    convenience before, but it should be roughly the same behavior with
    `char`.
    
    See relevant LLVM commits:
    
    https://github.com/llvm/llvm-project/commit/aeecef08c385b1e4955155dd649a2d3724463849
    
    https://github.com/llvm/llvm-project/commit/08a0faf4cd32bce6c51027ea9b5ec351747995b4

--- src/analyzer/protocol/ssl/SSL.cc
+++ src/analyzer/protocol/ssl/SSL.cc
@@ -32,11 +32,11 @@ static inline T LSB(const T a) {
     return (a & 0xff);
 }
 
-static std::basic_string<unsigned char> fmt_seq(uint32_t num) {
-    std::basic_string<unsigned char> out(4, '\0');
+static std::string fmt_seq(uint32_t num) {
+    std::string out(4, '\0');
     out.reserve(13);
     uint32_t netnum = htonl(num);
-    out.append(reinterpret_cast<u_char*>(&netnum), 4);
+    out.append(reinterpret_cast<char*>(&netnum), 4);
     out.append(5, '\0');
     return out;
 }
@@ -266,12 +266,12 @@ bool SSL_Analyzer::TryDecryptApplicationData(int len, const u_char* data, bool i
         // server write_key
         const u_char* s_wk = keys.data() + 32;
         // client IV
-        const u_char* c_iv = keys.data() + 64;
+        const char* c_iv = reinterpret_cast<const char*>(keys.data()) + 64;
         // server IV
-        const u_char* s_iv = keys.data() + 68;
+        const char* s_iv = reinterpret_cast<const char*>(keys.data()) + 68;
 
         // FIXME: should we change types here?
-        u_char* encrypted = (u_char*)data;
+        char* encrypted = (char*)data;
         size_t encrypted_len = len;
 
         if ( is_orig )
@@ -280,7 +280,7 @@ bool SSL_Analyzer::TryDecryptApplicationData(int len, const u_char* data, bool i
             s_seq++;
 
         // AEAD nonce, length 12
-        std::basic_string<unsigned char> s_aead_nonce;
+        std::string s_aead_nonce;
         if ( is_orig )
             s_aead_nonce.assign(c_iv, 4);
         else
@@ -306,14 +306,14 @@ bool SSL_Analyzer::TryDecryptApplicationData(int len, const u_char* data, bool i
 
         // FIXME: aes_256_gcm should not be hardcoded here ;)
         if ( is_orig )
-            EVP_DecryptInit(ctx, EVP_aes_256_gcm(), c_wk, s_aead_nonce.data());
+            EVP_DecryptInit(ctx, EVP_aes_256_gcm(), c_wk, reinterpret_cast<const u_char*>(s_aead_nonce.data()));
         else
-            EVP_DecryptInit(ctx, EVP_aes_256_gcm(), s_wk, s_aead_nonce.data());
+            EVP_DecryptInit(ctx, EVP_aes_256_gcm(), s_wk, reinterpret_cast<const u_char*>(s_aead_nonce.data()));
 
         EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, encrypted + encrypted_len);
 
         // AEAD tag
-        std::basic_string<unsigned char> s_aead_tag;
+        std::string s_aead_tag;
         if ( is_orig )
             s_aead_tag = fmt_seq(c_seq);
         else
@@ -330,8 +330,10 @@ bool SSL_Analyzer::TryDecryptApplicationData(int len, const u_char* data, bool i
                                              16); // see OpenSSL manpage - 16 is the block size for the supported cipher
         int decrypted_len = 0;
 
-        EVP_DecryptUpdate(ctx, NULL, &decrypted_len, s_aead_tag.data(), s_aead_tag.size());
-        EVP_DecryptUpdate(ctx, decrypted.data(), &decrypted_len, (const u_char*)encrypted, encrypted_len);
+        EVP_DecryptUpdate(ctx, NULL, &decrypted_len, reinterpret_cast<const u_char*>(s_aead_tag.data()),
+                          s_aead_tag.size());
+        EVP_DecryptUpdate(ctx, decrypted.data(), &decrypted_len, reinterpret_cast<const u_char*>(encrypted),
+                          encrypted_len);
         assert(static_cast<decltype(decrypted.size())>(decrypted_len) <= decrypted.size());
         decrypted.resize(decrypted_len);
 
